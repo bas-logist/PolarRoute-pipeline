fc2a2086d36b43378af6571049f16adbb320532a
Chris Corbin <chris.corbin@bas.ac.uk>
2026-09-24 14:12:59 +0100

    Remove region-specific mesh generation scripts which can now be handled by mesh_generate.sh with env variables.

--- scripts/mesh_generate_northern.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Get absolute path of pipeline directory and scripts directory
-pipeline_directory=$PIPELINE_DIRECTORY
-scripts_directory=$SCRIPTS_DIRECTORY
-
-bash $scripts_directory/mesh_generate.sh $pipeline_directory/configs/environment_configs/amsr_northern.config.json
